81ca26973618d304a5d6af706054d1f69bee02f2
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-01-16 13:16:13 +0200

    store subcription

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,18 @@
-import { Button } from "components/Button/Button";
-import css from "./StatusFilter.module.css";
+import { Button } from 'components/Button/Button';
+import css from './StatusFilter.module.css';
+
+import { useSelector } from 'react-redux';
+import { statusFilters } from 'redux/constants';
+import { getStatusFilter } from 'redux/selectors';
 
 export const StatusFilter = () => {
+  const filter = useSelector(getStatusFilter);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
